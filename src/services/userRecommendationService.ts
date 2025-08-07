b87c82ae1b99f5d0435343e7845fc52d57cda898
emily9821 <102820056+dltjwls02@users.noreply.github.com>
2025-08-07 23:04:39 +0900

    Update userRecommendationService.ts

--- src/services/userRecommendationService.ts
+++ src/services/userRecommendationService.ts
@@ -41,6 +41,16 @@ export class RecommendationService {
       }),
     ]);
 
+    //인기도 상승에 따른 알림 생성
+    await prisma.notification.create({
+    data: {
+      fromUserId: userId,
+      toUserId: dto.targetUserId,
+      type: 'popularityUp',
+      title: `인기도가 1 상승하였습니다.`,
+    },
+  },);
+
     return {
       success: true,
       message: '추천을 보냈습니다.',
